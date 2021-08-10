88c834b401db6a9889a5f54f894930ae6b7beaa1
Faisal Ramdan <faisalramdan.id@gmail.com>
2021-08-10 10:41:10 +0700

    Updated

--- lib/modules/intro_screen.dart
+++ lib/modules/intro_screen.dart
@@ -10,32 +10,75 @@ class IntroScreenModule extends StatefulWidget {
 
 class _IntroScreenModuleState extends State<IntroScreenModule> {
   final introKey = GlobalKey<IntroductionScreenState>();
+  late List<PageViewModel> listPagesViewModel;
 
-  void _onIntroEnd(context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (_) => HomeScreen()),
-    );
-  }
-
-  Widget _buildFullscrenImage() {
-    return Image.asset(
-      'assets/intro-screen/fullscreen.jpg',
-      fit: BoxFit.cover,
-      height: double.infinity,
-      width: double.infinity,
-      alignment: Alignment.center,
-    );
-  }
-
-  Widget _buildImage(String assetName, [double width = 350]) {
-    return Image.asset('assets/intro-screen/$assetName', width: width);
+  @override
+  void initState() {
+    initVariables();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    return IntroductionScreen(
+      key: introKey,
+      pages: listPagesViewModel,
+      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+      onDone: () => _onIntroEnd(context),
+      next: const Icon(Icons.arrow_forward),
+      skip: const Text('Skip'),
+      globalHeader: Align(
+        alignment: Alignment.topRight,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16, right: 16),
+            child: _buildImage('flutter.png', 100),
+          ),
+        ),
+      ),
+      globalFooter: SizedBox(
+        width: double.infinity,
+        height: 60,
+        child: ElevatedButton(
+          child: const Text(
+            'Let\s go right away!',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () => _onIntroEnd(context),
+        ),
+      ),
+      globalBackgroundColor: Colors.white,
+      showSkipButton: true,
+      // onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      skipFlex: 0,
+      nextFlex: 0,
+      //rtl: true, // Display as right-to-left
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(16),
+      controlsPadding: kIsWeb
+          ? const EdgeInsets.all(12.0)
+          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      dotsContainerDecorator: const ShapeDecoration(
+        color: Colors.black87,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+      ),
+    );
+  }
+
+  void initVariables() {
     const bodyStyle = TextStyle(fontSize: 19.0);
 
-    const pageDecoration = const PageDecoration(
+    const pageDecoration = PageDecoration(
       titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
       bodyTextStyle: bodyStyle,
       descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
@@ -43,7 +86,7 @@ class _IntroScreenModuleState extends State<IntroScreenModule> {
       imagePadding: EdgeInsets.zero,
     );
 
-    List<PageViewModel> listPagesViewModel = [
+    listPagesViewModel = [
       PageViewModel(
         title: "Fractional shares",
         body:
@@ -118,60 +161,25 @@ class _IntroScreenModuleState extends State<IntroScreenModule> {
         reverse: true,
       ),
     ];
+  }
 
-    return IntroductionScreen(
-      key: introKey,
-      pages: listPagesViewModel,
-      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
-      onDone: () => _onIntroEnd(context),
-      next: const Icon(Icons.arrow_forward),
-      skip: const Text('Skip'),
-      globalHeader: Align(
-        alignment: Alignment.topRight,
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 16, right: 16),
-            child: _buildImage('flutter.png', 100),
-          ),
-        ),
-      ),
-      globalFooter: SizedBox(
-        width: double.infinity,
-        height: 60,
-        child: ElevatedButton(
-          child: const Text(
-            'Let\s go right away!',
-            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-          ),
-          onPressed: () => _onIntroEnd(context),
-        ),
-      ),
-
-      globalBackgroundColor: Colors.white,
-      showSkipButton: true,
-      // onSkip: () => _onIntroEnd(context), // You can override onSkip callback
-      skipFlex: 0,
-      nextFlex: 0,
-      //rtl: true, // Display as right-to-left
-      curve: Curves.fastLinearToSlowEaseIn,
-      controlsMargin: const EdgeInsets.all(16),
-      controlsPadding: kIsWeb
-          ? const EdgeInsets.all(12.0)
-          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-      dotsDecorator: const DotsDecorator(
-        size: Size(10.0, 10.0),
-        color: Color(0xFFBDBDBD),
-        activeSize: Size(22.0, 10.0),
-        activeShape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(25.0)),
-        ),
-      ),
-      dotsContainerDecorator: const ShapeDecoration(
-        color: Colors.black87,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-        ),
-      ),
+  void _onIntroEnd(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => HomeScreen()),
     );
+  }
+
+  Widget _buildFullscrenImage() {
+    return Image.asset(
+      'assets/intro-screen/fullscreen.jpg',
+      fit: BoxFit.cover,
+      height: double.infinity,
+      width: double.infinity,
+      alignment: Alignment.center,
+    );
+  }
+
+  Widget _buildImage(String assetName, [double width = 350]) {
+    return Image.asset('assets/intro-screen/$assetName', width: width);
   }
 }
